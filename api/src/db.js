b8e8e57655bf45304f7a7f47751571bbc4db712a
Fran Andrade <franandrade15994@gmail.com>
2023-02-16 19:06:06 -0300

    Api

--- api/src/db.js
+++ api/src/db.js
@@ -4,9 +4,9 @@ const Product = require('./moduls/Products.js');
 const Types = require('./moduls/Types.js');
 const Users = require('./moduls/Users.js');
 
-const { USER_DB, PASSWORD_DB, HOST_DB, NAME_DB} = process.env;
+const { USER_DB, PASSWORD_DB, HOST_DB, NAME_DB, DB_DEPLOY} = process.env;
 
-const sequelize = new Sequelize(`postgres://${USER_DB}:${PASSWORD_DB}@${HOST_DB}/${NAME_DB}`, {
+const sequelize = new Sequelize(DB_DEPLOY, {
     dialect: 'postgres',
     logging: false,
     native: false
